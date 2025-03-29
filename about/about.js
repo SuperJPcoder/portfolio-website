5bcce601ee61cd203078b0c464e770899b677f07
SuperJPcoder <priyanksjhaveri@gmail.com>
2025-03-30 00:38:21 +0530

    about page done except name anima

--- about/about.js
+++ about/about.js
@@ -4,24 +4,10 @@ setInterval(() => {
   document.getElementById('traits').innerText = traitsList[traitIndex];
   traitIndex = (traitIndex + 1) % traitsList.length;
 }, 1000);
-setTimeout(() => {
-  document.getElementById('spacebar-text').classList.remove('hidden');
-}, 3000);
+
 window.onload = () => {
   setTimeout(() => {
     document.querySelector('.name-animation').style.display = 'none';
     document.querySelector('.container').classList.add('show');
   }, 3200);
 };
-document.addEventListener('keydown', (e) => {
-  if (e.code === 'Space') {
-    navigateToHome();
-  }
-});
-function navigateToHome() {
-  document.querySelector('.container').style.opacity = '0';
-  document.querySelector('.container').style.transform = 'translateY(-20px)';
-  setTimeout(() => {
-    window.location.href = '../landing/landing.html';
-  }, 600);
-}
