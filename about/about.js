2a1a8b27348da0a2c523a3c340338d4dee1ad914
SuperJPcoder <priyanksjhaveri@gmail.com>
2025-03-30 19:10:12 +0530

    inital eqns display, animation to traits

--- about/about.js
+++ about/about.js
@@ -1,16 +1,23 @@
 const traitsList = ["polite", "humble", "considerate", "helpful", "modest", "mannered", "cultured"];
 let traitIndex = 0;
-setInterval(() => {
-  document.getElementById('traits').innerText = traitsList[traitIndex];
-  traitIndex = (traitIndex + 1) % traitsList.length;
-}, 1000);
+const containerElement = document.getElementById("traits-container");
+const traitsElement = document.getElementById("traits");
 
-const video = document.getElementById('intro-video');
-video.onended = () => {
-  document.querySelector('.video-container').style.display = 'none';
-  const container = document.querySelector('.container');
-  container.classList.remove('hidden');
+setInterval(() => {
+  containerElement.classList.add("flip");
   setTimeout(() => {
-    container.classList.add('show');
-  }, 100); 
+    traitIndex = (traitIndex + 1) % traitsList.length;
+    traitsElement.innerText = traitsList[traitIndex];
+    containerElement.classList.remove("flip");
+  }, 300);
+}, 2000);
+
+const video = document.getElementById("intro-video");
+video.onended = () => {
+  document.querySelector(".video-container").style.display = "none";
+  const container = document.querySelector(".container");
+  container.classList.remove("hidden");
+  setTimeout(() => {
+    container.classList.add("show");
+  }, 100);
 };
